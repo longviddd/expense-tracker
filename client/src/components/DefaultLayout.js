cad9b861e6fb75357831c2a1852793bbb36b7558
Long Nguyen <rongdat1710@gmail.com>
2022-11-23 10:21:59 -0500

    finished header of default layout

---
+++ client/src/components/DefaultLayout.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../resources/default-layout.css";
+function DefaultLayout(props) {
+  return (
+    <div className="layout">
+      <div className="header d-flex justify-content-between align-items-center">
+        <div>
+          <h1 className="logo">LONG MONEY</h1>
+        </div>
+        <div>
+          <h1 className="username">Username: </h1>
+        </div>
+      </div>
+      <div className="content">{props.children}</div>
+    </div>
+  );
+}
+
+export default DefaultLayout;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import DefaultLayout from "../components/DefaultLayout";
+
+export default function Home() {
+  return (
+    <DefaultLayout>
+      <h1>This is home page</h1>
+    </DefaultLayout>
+  );
+}
