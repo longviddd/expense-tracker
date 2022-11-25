5c707a051fc190c3770c23bb853e9251e7ab3067
Long Nguyen <rongdat1710@gmail.com>
2022-11-25 13:29:53 -0500

    create layout and modal for transaction view

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,26 @@
-import React from "react";
+import { Modal } from "antd";
+import React, { useState } from "react";
 import DefaultLayout from "../components/DefaultLayout";
+import "../resources/transactions.css";
 
 export default function Home() {
+  const [showModal, setShowModal] = useState(false);
   return (
     <DefaultLayout>
-      <h1>This is home page</h1>
+      <div className="filter d-flex justify-content-between align-items-center">
+        <div></div>
+        <div>
+          <button className="primary" onClick={() => setShowModal(true)}>
+            ADD NEW
+          </button>
+        </div>
+      </div>
+      <div className="table-analytics"></div>
+      <Modal
+        title="Add Transaction"
+        visible={showModal}
+        onCancel={() => setShowModal(false)}
+      ></Modal>
     </DefaultLayout>
   );
 }
