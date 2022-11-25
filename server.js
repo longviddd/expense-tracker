8c33a72e1b7526e9c41850839648cc0e3dc16169
Long Nguyen <rongdat1710@gmail.com>
2022-11-25 09:01:15 -0500

    finish base user endpoints

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const dbConnect = require("./dbConnect");
 const app = express();
+app.use(express.json());
 const port = 5000;
+const userRoute = require("./routes/usersRoute");
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use("/api/users/", userRoute);
 app.listen(port, () => console.log(`Node JS server started at port ${port}!`));
